208e0f67596150f465a273b8f030e4c88ccbf033
Alfonsosoriano <asorrod2604@g.educaand.es>
2025-12-14 21:07:39 +0100

    Import csv y funcion LeerPartidos()
    He puesto el import csv para poder leerlo con la función leerPartidos(), esta función se utiliza para coger del fichero csv y devuelve una lista de diccionarios.

--- liga.py
+++ liga.py
@@ -1,1 +1,14 @@
+import csv
 
+def LeerPartidos(ruta="liga.csv"):
+    """Lee el fichero CSV y devuelve una lista de diccionarios."""
+    partidos = []
+    with open(ruta, newline='', encoding='utf-8') as f:
+        lector = csv.reader(f)
+        encabezados = next(lector) 
+        for fila in lector:
+            partido = {}
+            for i in range(len(encabezados)):
+                partido[encabezados[i]] = fila[i]
+            partidos.append(partido)
+    return partidos
